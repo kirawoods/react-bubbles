2f754e19184bab221b73e8f68314bfb9081962f6
Kira <kawoods826@gmail.com>
2020-03-11 13:33:51 -0700

    adds route for bubble page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BubblePage } from "./components/BubblePage";
 
 import Login from "./components/Login";
 import "./styles.scss";
@@ -9,6 +10,7 @@ function App() {
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
+        <Route exact path="/bubbles" component={BubblePage} />
         {/* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
-const BubblePage = () => {
+export const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
@@ -16,5 +16,3 @@ const BubblePage = () => {
     </>
   );
 };
-
-export default BubblePage;
