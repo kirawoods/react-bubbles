afec33be784e7d78b1a1dcb84adf959144f429a7
Kira <kawoods826@gmail.com>
2020-03-11 13:51:42 -0700

    adds authentication

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import { AxiosWithAuth } from "../utils/AxiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
-  return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
-};
+export class Login extends Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
 
-export default Login;
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    AxiosWithAuth()
+      .post("/login", this.state.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/bubbles");
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    return (
+      <div className="login-form">
+        <form onSubmit={this.login}>
+          <label>Username</label>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
